32d4fc3dd751149d73de1ca2cf2697257bb7339a
johneiser <johneiser@icloud.com>
2020-11-14 18:42:54 -0500

    1.0.2

--- lets/redirect/socat.py
+++ lets/redirect/socat.py
@@ -5,6 +5,10 @@ import argparse
 class Socat(Module):
     """
     Redirect traffic with socat.
+        - requires two '--' before socat commands
+
+    Example:
+    > lets redirect/socat -- -- -dd -v tcp-listen:4444,fork,reuseaddr tcp-connect:127.0.0.1:4445
     """
     images = [
         "local/socat",      # Cross-platform
@@ -12,19 +16,23 @@ class Socat(Module):
 
     @classmethod
     def add_arguments(self, parser):
-        parser.add_argument("command", nargs=argparse.REMAINDER,
-            help="command to provide socat")
+        parser.add_argument("command", nargs="*", help="command(s) to provide socat")
 
-    def handle(self, input, command):
+    def handle(self, input, command=None):
         
         # Launch redirector
         with Container.run("local/socat",
             network="host",     # Use host network to allow local addresses
             tty=True,
             stdin_open=True,
+            entrypoint="socat",
             command=command) as container:
 
+            # Print any missed logs
+            self.log.logger.info(container.logs().decode())
+
             container.interact()
+
 
 class SocatTestCase(TestCase):
     images = ["local/socat"]
